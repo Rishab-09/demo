2587f9094068fd8032f4222a479b5c36100cd4e1
Rishab <80740536+Rishab-09@users.noreply.github.com>
2022-07-12 15:19:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Library</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div id="logo">
+	<img src="image/img2.png" width="80" height="90">
+	</div>
+<div id="header">
+	<h1  style="padding: 20px"><font color="black" size="20"><b>DYPIU LIBRARY</b></font></h1>
+</div>
+<span><marquee>Welcome to DYPIU library. Library opens at 9:00 AM and close at 5:00 PM</marquee></span>
+<div id="nav">
+	<ul>
+		<li ><h3><a style="text-decoration: none; color: black"  href="sign up.php">Sign Up</a></h3></li>
+
+		<li><h3><a style="text-decoration: none; color: black" href="sign in.php">Log In</a></h3></li>
+
+		<li><h3><a style="text-decoration: none; color: black" href="admin.php">Admin</a></h3></li>
+		
+	</ul>
+</div>
+
+<div id="container">
+	<img src="image/1.jpg" width="100%" height="800">
+	<div id="left"><br><br><br><br>
+		<center><img src="image/img0.png" width="600" height="350"></center><br>	
+	</div>
+	<div id="right" style="text-align: center;"><br><br>
+		<h1><font size="12" color="green"><b>Category</b></font></h1><br><br>
+		<font color="blue" size="5">
+		<b>
+		<a style="text-decoration: none;" href="btech.php">B-Tech</a><br><br>
+		<a style="text-decoration: none;" href="biotech.php">Bio-Tech</a><br><br>
+		<a style="text-decoration: none;" href="biog.php">Biography</a><br><br>
+		<a style="text-decoration: none;" href="poet.php">Poetry</a><br><br>
+		<a style="text-decoration: none;" href="ency.php">Encyclopedia</a></b><br><br>
+		</font>
+	</div><br><br>
+	<h1 style="text-align: center;">VISIT FOR MORE BOOKS</h1>
+	<iframe src="https://openlibrary.org///" target="_blank" frameborder="0" width="100%" height="650">></iframe>
+</div>
+<div id="footer"><br><br>
+	<h3 style="text-align: center"><font size="7" color="red"><b>Why Online Library?</b></font></h3>
+	<div class="about-section"><br>
+	  <p>Online Library Management System is an Automated Library System that handles the various functions of the library which helps the students in better learning by providing quick access to find books, catalogs, magazines of their interest.</p>
+	  <p>The library is a great place to study and make notes which ultimately help in scoring a good score.</p><br>
+	  <b><p>"I have always imagined that Paradise will be a kind of a Library."</p></b><br>
+	</div><br><br><br>
+	<h4><font size="6.5">About Myself</font></h4>
+	<div class="row">
+		<div class="column">
+			<div class="card">
+				<img src="image/rishab.jpeg" alt="Jane" style="width:95%">
+				<div class="container">
+			    	<h2>Rishab Jha</h2>
+			    	<p>B-Tech (CSE) Student, 2nd year</p>
+			    	<p>E-mail ID: 20190802072@dypiu.ac.in</p>
+		    	</div>
+			</div>
+		</div>
+	</div>
+	<hr style="background-color: black">
+	<font size="3"><p style="text-align: center;">Copyright © 2021, Inc. All Right Reserved</p></font>
+</div>
+</body>
+</html>
+
